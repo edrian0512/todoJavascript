5c24d75c4fdb0fc5bd308c085325c6686ab59bf4
Edrian <mr.edrian@gmail.com>
2023-03-24 14:07:14 +0800

    Add Filter

--- script.js
+++ script.js
@@ -25,28 +25,6 @@ function addItem() {
   }
 }
 
-var storedList = JSON.parse(localStorage.getItem("list"));
-if (storedList != null) {
-  list = storedList;
-  displayList();
-}
-
-function displayList() {
-  var html = "";
-  for (var i = 0; i < list.length; i++) {
-    html +=
-      "<li class='list-group-item mb-2 shadow-lg' style='max-width: 100%; word-wrap: break-word;'>" +
-      list[i] +
-      "<button type='button' class='close ml-1' onclick='deleteItem(" +
-      i +
-      ")'><i class='fa fa-trash btn btn-warning'></i></button>" +
-      "<button type='button' class='close ml-3 ' onclick='editItem(" +
-      i +
-      ")'><i class='fa fa-edit btn btn-danger' ></i></button></li>";
-  }
-  document.getElementById("list").innerHTML = html;
-}
-
 function editItem(index) {
   var modal = document.getElementById("myModal");
   var newItemInput = document.getElementById("newItemInput");
@@ -94,4 +72,83 @@ function exit() {
   localStorage.removeItem("list");
   localStorage.clear();
   window.location.href = "index.html";
+}
+
+function displayList() {
+  var html = "";
+  for (var i = 0; i < list.length; i++) {
+    html +=
+      "<li class='list-group-item mb-2 shadow-lg' style='max-width: 100%; word-wrap: break-word;'>" +
+      "<span class='todo-item'>" +
+      list[i] +
+      "</span>" +
+      "<button type='button' class='close ml-1' onclick='deleteItem(" +
+      i +
+      ")'><i class='fa fa-trash btn btn-warning'></i></button>" +
+      "<button type='button' class='close ml-1' onclick='editItem(" +
+      i +
+      ")'><i class='fa fa-edit btn btn-danger'></i></button>" +
+      "<button type='button' class='close' onclick='markDone(" +
+      i +
+      ")'><i class='fa fa-check btn btn-success'></i></button>" +
+      "</li>";
+  }
+  document.getElementById("list").innerHTML = html;
+}
+
+function markDone(index) {
+  if (list[index].startsWith("<del>")) {
+    list[index] = list[index].substring(5, list[index].length - 6);
+  } else {
+    list[index] = "<del>" + list[index] + "</del>";
+  }
+  displayList();
+  localStorage.setItem("list", JSON.stringify(list));
+}
+
+function filterList() {
+  var select = document.getElementById("filterSelect");
+  var option = select.value;
+
+  if (option == "all") {
+    displayList();
+  } else if (option == "completed") {
+    var filteredList = list.filter(function (item) {
+      return item.startsWith("<del>");
+    });
+    displayFilteredList(filteredList);
+  } else if (option == "uncompleted") {
+    var filteredList = list.filter(function (item) {
+      return !item.startsWith("<del>");
+    });
+    displayFilteredList(filteredList);
+  }
+}
+
+function displayFilteredList(filteredList) {
+  var html = "";
+  for (var i = 0; i < filteredList.length; i++) {
+    html +=
+      "<li class='list-group-item mb-2 shadow-lg' style='max-width: 100%; word-wrap: break-word;'>" +
+      "<span class='todo-item'>" +
+      filteredList[i] +
+      "</span>" +
+      "<button type='button' class='close ml-1' onclick='deleteItem(" +
+      i +
+      ")'><i class='fa fa-trash btn btn-warning'></i></button>" +
+      "<button type='button' class='close ml-1' onclick='editItem(" +
+      i +
+      ")'><i class='fa fa-edit btn btn-danger'></i></button>" +
+      "<button type='button' class='close' onclick='markDone(" +
+      i +
+      ")'><i class='fa fa-check btn btn-success'></i></button>" +
+      "</li>";
+  }
+  document.getElementById("list").innerHTML = html;
+}
+
+var storedList = JSON.parse(localStorage.getItem("list"));
+if (storedList != null) {
+  list = storedList;
+  filterList();
 }
